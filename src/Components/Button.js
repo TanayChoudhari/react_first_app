8dd68db113cd44f831f3d255f36ce6e2bd022a6e
Tanay <tanaydemoacc123@gmail.com>
2024-11-27 21:03:03 +0530

    Initial Commit

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component
+{
+  	render()
+	{
+    	return <button className="Button">This is Btn</button>
+  	}
+}
+
+export default Button;
